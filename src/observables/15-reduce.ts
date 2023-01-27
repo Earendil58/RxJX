8513f674ca62d60195cba936ac398696136591bb
Gonzalo Aguirre <gonzalo5aguirre9@gmail.com>
2023-01-26 23:20:44 -0300

    operador reduce

---
+++ src/observables/15-reduce.ts
@@ -0,0 +1,22 @@
+import { interval, range, reduce, take, tap } from 'rxjs';
+
+
+const numbers = [1,2,3,4,5];
+
+const totalReducer = (acumulador, valorActual) => {
+    return acumulador + valorActual;
+};
+
+const total = numbers.reduce(totalReducer, 0);
+
+console.log('total', total);
+
+interval(1000).pipe(
+    take(4),
+    tap(x => console.log('antes', x)),
+    reduce( totalReducer )
+)
+    .subscribe({
+        next: val => console.log('next', val),
+        complete: () => console.log('Complete'),
+    })
